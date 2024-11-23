78db27aa105b525614052b4fe3e002602faaf1a0
khalil chermiti <khalil.chermiti@proton.me>
2024-11-23 09:20:56 +0100

    fix: remove description from products list page

--- client/app/components/Store/ProductList/index.js
+++ client/app/components/Store/ProductList/index.js
@@ -4,38 +4,38 @@
  *
  */
 
-import React from 'react';
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import AddToWishList from '../AddToWishList';
-import Button from '../../Common/Button';
+import AddToWishList from "../AddToWishList";
+import Button from "../../Common/Button";
 
-const ProductList = props => {
+const ProductList = (props) => {
   const { products, updateWishlist, authenticated } = props;
 
   function getWishlist() {
-    return JSON.parse(localStorage.getItem('wishlist')) || [];
+    return JSON.parse(localStorage.getItem("wishlist")) || [];
   }
 
   function isLiked(id) {
     const res =
-      getWishlist().findIndex(el => el.toString() === id.toString()) != -1
+      getWishlist().findIndex((el) => el.toString() === id.toString()) != -1
         ? true
         : false;
 
-    console.log('product with id : ' + id + 'is ', res ? 'liked' : 'not liked');
+    console.log("product with id : " + id + "is ", res ? "liked" : "not liked");
 
     return res;
   }
 
   return (
-    <div className='product-list'>
+    <div className="product-list">
       {products.map((product, index) => (
-        <div key={index} className='mb-3 mb-md-0'>
-          <div className='product-container'>
-            <div className='item-box'>
-              <div className='add-wishlist-box'>
+        <div key={index} className="mb-3 mb-md-0">
+          <div className="product-container">
+            <div className="item-box">
+              <div className="add-wishlist-box">
                 <AddToWishList
                   id={product._id}
                   liked={isLiked(product._id)}
@@ -45,61 +45,61 @@ const ProductList = props => {
                 />
               </div>
 
-              <div className='item-link'>
+              <div className="item-link">
                 <Link
                   to={`/product/${product.slug}`}
-                  className='d-flex flex-column h-100'
+                  className="d-flex flex-column h-100"
                 >
-                  <div className='item-image-container'>
-                    <div className='item-image-box'>
+                  <div className="item-image-container">
+                    <div className="item-image-box">
                       <img
-                        className='item-image'
+                        className="item-image"
                         src={`${
                           product.imageUrl
                             ? product.imageUrl
-                            : '/images/placeholder-image.png'
+                            : "/images/placeholder-image.png"
                         }`}
                       />
                     </div>
                   </div>
-                  <div className='item-body'>
-                    <div className='item-details p-3'>
-                      <h1 className='item-name'>{product.name}</h1>
+                  <div className="item-body">
+                    <div className="item-details p-3">
+                      <h1 className="item-name">{product.name}</h1>
                       {product.brand &&
                         Object.keys(product.brand).length > 0 && (
-                          <p className='by'>
+                          <p className="by">
                             Par <span>{product.brand.name}</span>
                           </p>
                         )}
-                      <div className='ql-editor'>
+                      <div className="ql-editor">
                         <p
                           style={{
-                            overflow: 'hidden',
-                            display: '-webkit-box',
-                            WebkitBoxOrient: 'vertical',
+                            overflow: "hidden",
+                            display: "-webkit-box",
+                            WebkitBoxOrient: "vertical",
                             WebkitLineClamp: 2,
-                            textOverflow: 'ellipsis'
+                            textOverflow: "ellipsis",
                           }}
-                          className='mb-2  '
-                          dangerouslySetInnerHTML={{
-                            __html: product.description
-                          }}
+                          className="mb-2  "
+                          // dangerouslySetInnerHTML={{
+                          //   __html: product.description,
+                          // }}
                         />
                       </div>
                     </div>
                   </div>
-                  <div className='d-flex flex-row justify-content-between align-items-center px-4 mb-2 item-footer'>
-                    <p className='price mb-0'>{product.price} DT</p>
+                  <div className="d-flex flex-row justify-content-between align-items-center px-4 mb-2 item-footer">
+                    <p className="price mb-0">{product.price} DT</p>
                     {product.totalReviews > 0 && (
-                      <p className='mb-0'>
-                        <span className='fs-16 fw-normal mr-1'>
+                      <p className="mb-0">
+                        <span className="fs-16 fw-normal mr-1">
                           {parseFloat(product?.averageRating).toFixed(1)}
                         </span>
                         <span
                           className={`fa fa-star ${
-                            product.totalReviews !== 0 ? 'checked' : ''
+                            product.totalReviews !== 0 ? "checked" : ""
                           }`}
-                          style={{ color: '#ffb302' }}
+                          style={{ color: "#ffb302" }}
                         ></span>
                       </p>
                     )}
