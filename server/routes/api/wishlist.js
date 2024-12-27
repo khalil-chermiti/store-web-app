a82b097f6a8248a7b8da72df5962eedd2d375327
khalil chermiti <khalil.chermiti@proton.me>
2024-12-27 16:14:39 +0100

    fix: fix wishlist not fetching products list

--- server/routes/api/wishlist.js
+++ server/routes/api/wishlist.js
@@ -1,37 +1,37 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Bring in Models & Helpers
-const Wishlist = require('../../models/wishlist');
-const Product = require('../../models/product');
-const auth = require('../../middleware/auth');
+const Wishlist = require("../../models/wishlist");
+const Product = require("../../models/product");
+const auth = require("../../middleware/auth");
 
-router.post('/', auth, async (req, res) => {
+router.post("/", auth, async (req, res) => {
   try {
     const { product, isLiked } = req.body;
     const user = req.user;
     const update = {
       product,
       isLiked,
-      updated: Date.now()
+      updated: Date.now(),
     };
     const query = { product: update.product, user: user._id };
 
     const updatedWishlist = await Wishlist.findOneAndUpdate(query, update, {
-      new: true
+      new: true,
     });
 
     if (updatedWishlist !== null) {
       res.status(200).json({
         success: true,
-        message: 'Your Wishlist has been updated successfully!',
-        wishlist: updatedWishlist
+        message: "Your Wishlist has been updated successfully!",
+        wishlist: updatedWishlist,
       });
     } else {
       const wishlist = new Wishlist({
         product,
         isLiked,
-        user: user._id
+        user: user._id,
       });
 
       const wishlistDoc = await wishlist.save();
@@ -39,35 +39,32 @@ router.post('/', auth, async (req, res) => {
       res.status(200).json({
         success: true,
         message: `Added to your Wishlist successfully!`,
-        wishlist: wishlistDoc
+        wishlist: wishlistDoc,
       });
     }
   } catch (e) {
     return res.status(400).json({
-      error: 'Your request could not be processed. Please try again.'
+      error: "Your request could not be processed. Please try again.",
     });
   }
 });
 
 // FIXME
 // fetch wishlist api Test
-router.post('/list', async (req, res) => {
+router.post("/list", async (req, res) => {
   try {
     const productIds = req.body.productIds;
 
-    const products = await Product.find({ _id: { $in: productIds } })
-      .populate({
-        path: 'product',
-        select: 'name slug price imageUrl'
-      })
-      .sort('-updated');
+    const products = await Product.find({ _id: { $in: productIds } }).sort(
+      "-updated",
+    );
 
     return res.status(200).json({
-      wishlist: products
+      wishlist: products,
     });
   } catch (error) {
     return res.status(400).json({
-      error: 'Your request could not be processed. Please try again.'
+      error: "Your request could not be processed. Please try again.",
     });
   }
 });
